d85e61aacdb8e33c43923d4225c1353db681ff2c
a1215439436 <1215439436@qq.com>
2023-09-29 23:29:09 +0800

    idea menu菜单

--- src/apps/idea/components/HeadMenu.vue
+++ src/apps/idea/components/HeadMenu.vue
@@ -1,34 +1,61 @@
 <template>
+<!--  主题-->
+  <a-config-provider
+      :theme="{ token: { colorPrimary: data.colorPrimary, borderRadius: `${data.borderRadius}px` } }"
+  >
   <div class="backgroundColorDark backgroundColorDarkHeader" style="display: flex;height: 35px;align-items: center">
     <img style="width: 17px;height:17px;margin-left: 6px;margin-right: 10px;" src="public/icon/idea.webp" alt="">
-    <div class="menu" style="width: 17px;height: 17px">
-      <a-cascader :options="options">
+    <a-dropdown name="theme" :trigger="['click']">
+      <a class="ant-dropdown-link" @click.prevent>
         <MenuUnfoldOutlined />
-      </a-cascader>
-
-    </div>
+      </a>
+      <template #overlay>
+        <a-menu style="width: 300px;">
+          <a-sub-menu key="new" title="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;new">
+            <a-menu-item><span class="menu-item-icon"></span>Project...</a-menu-item>
+            <a-menu-item><span class="menu-item-icon"></span>Project from Version Control...</a-menu-item>
+            <a-menu-item><AlignLeftOutlined /> &nbsp;File</a-menu-item>
+            <a-menu-item><span class="menu-item-icon"></span>Scratch File</a-menu-item>
+            <a-menu-item><FolderOutlined /> &nbsp;Directory</a-menu-item>
+            <a-menu-item><span style="font-size: 8px;color: green;">&lt;&gt;</span>&nbsp;HTML File</a-menu-item>
+          </a-sub-menu>
+          <a-menu-item><FolderOutlined /> &nbsp;Open...</a-menu-item>
+          <a-menu-item><span class="menu-item-icon"></span>Save As...<span class="menu-placeholder">Ctrl+Shift+S</span></a-menu-item>
+          <a-menu-item><span class="menu-item-icon"></span>Attach project...</a-menu-item>
+          <a-sub-menu key="Recent Projects..." title="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Recent Projects...">
+          </a-sub-menu>
+        </a-menu>
+      </template>
+    </a-dropdown>
   </div>
+  </a-config-provider>
 </template>
 <script lang="ts" setup>
-import {MenuUnfoldOutlined} from "@ant-design/icons-vue";
+import {AlignLeftOutlined, MenuUnfoldOutlined, FolderOutlined} from "@ant-design/icons-vue";
 import '../css/Theme.css'
-
-const options = [
-  {
-    label:'new',
-    children:[
-      {label:'Project...'},
-      {label:'Project from version Control...'},
-      {label:''}
-    ]
-  }
-]
-
+import {reactive, ref} from "vue";
+import {theme} from "ant-design-vue";
+const defaultData = {
+  borderRadius: 6,
+  colorPrimary: '#1677ff',
+};
+const data = ref(defaultData);
+const { token } = theme.useToken();
 </script>
 <style scoped>
 .menu:hover{
   background-color: #2b2d30;
 }
 
-
+.menu-item-icon::before {
+  content: "";
+  display: inline-block;
+  width: 22px; /* 根据图标的宽度调整 */
+  height: 1px; /* 根据图标的高度调整 */
+  vertical-align: middle;
+}
+.menu-placeholder{
+  float: right;
+  color: #3c3f40;
+}
 </style>
