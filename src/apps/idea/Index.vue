a39db26369aeb95d5744b82d5dc517f33cc0e69c
a1215439436 <1215439436@qq.com>
2023-10-23 22:55:59 +0800

    1

--- src/apps/idea/Index.vue
+++ src/apps/idea/Index.vue
@@ -1,8 +1,21 @@
 <template>
   <head-menu></head-menu>
-  <content></content>
+  <div style="display: flex;justify-content: center;align-items: center;">
+    <content style="height: 50%"></content>
+  </div>
 </template>
 <script lang="ts" setup>
 import HeadMenu from "@/apps/idea/components/HeadMenu.vue";
 import Content from "@/apps/idea/components/Content.vue";
 </script>
+<style scoped lang="less">
+div {
+  height: 150vh;
+  background-color: #2b2d30;
+}
+
+content {
+  height: 100%;
+  flex-grow: 1;
+}
+</style>
--- src/apps/idea/components/Content.vue
+++ src/apps/idea/components/Content.vue
@@ -1,11 +1,24 @@
 <template>
-  <div>
-    <p>Search Everywhere</p>
+  <div style="">
+    <p><span class="left-text">Search Everywhere &nbsp;</span><span class="menu-placeholder">Double Shift</span></p>
+    <p><span class="left-text">Project View &nbsp;</span><span class="menu-placeholder">Alt+1</span></p>
+    <p><span class="left-text">Go to File &nbsp;</span><span class="menu-placeholder">Ctrl+Shift+N</span></p>
+    <p><span class="left-text">Recent Files &nbsp;</span><span class="menu-placeholder">Ctrl+E</span></p>
+    <p><span class="left-text">Navigation Bar &nbsp;</span><span class="menu-placeholder">Alt+Home</span></p>
+    <p><span class="left-text">Drop files here to open them</span></p>
+
   </div>
 </template>
 <script setup lang="ts">
 
 </script>
 <style scoped lang="less">
-
+.menu-placeholder{
+  color: #5f6465;
+}
+.left-text{
+  color: white;
+  float: left;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+}
 </style>
