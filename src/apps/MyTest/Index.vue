9c42391f5dfef6f7b38de28cfea1b35de7e82b86
悠米° <yuumigift@qq.com>
2024-01-09 22:21:21 +0800

    -

--- src/apps/MyTest/Index.vue
+++ src/apps/MyTest/Index.vue
@@ -1,14 +1,14 @@
 <template>
-  <div style="padding: 1em">
-    <h2>测试程序</h2>
-    <p>Hello Yuumi</p>
-    <div>
-      <input type="text" />
-    </div>
-    <div style="margin: auto; width: 300px; height: 300px">
-      <img src="/icon/garden.png" alt="" />
-    </div>
+  <div>
+    
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { promiseTimeout as sleep } from "@vueuse/core";
+
+
+
+
+
+</script>
