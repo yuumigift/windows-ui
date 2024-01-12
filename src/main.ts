80b2678530a0f2ce6fbb1b5e3c41bca8596f1626
悠米° <yuumigift@qq.com>
2024-01-12 14:53:45 +0800

    -

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'ant-design-vue/dist/reset.css';
+import "element-plus/dist/index.css"
 const app = createApp(App)
 
 app.use(router)
