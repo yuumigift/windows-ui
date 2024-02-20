119a36421ec780d85744060618fd3bc2d9656141
悠米° <yuumigift@qq.com>
2024-02-20 11:39:47 +0800

    reset

--- src/apps/MyTest/Index.vue
+++ src/apps/MyTest/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="box"></div>
+
   </div>
 </template>
 
@@ -8,23 +8,8 @@
 import { promiseTimeout as sleep } from "@vueuse/core";
 import { ElButton } from "element-plus";
 
-const my_width = ref(100);
 
-const withPx = (num: number) => `${num}px`;
-const act = () => {
-  my_width.value += 1;
-  requestAnimationFrame(act);
-};
 
-act();
+
+
 </script>
-
-<style lang="less" scoped>
-.box {
-  position: absolute;
-  left: 0;
-  width: v-bind("withPx(my_width)");
-  height: 50px;
-  background: #fae;
-}
-</style>
