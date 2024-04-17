dd336c63c99c684a3bdf750b27b2502074548fcf
悠米° <yuumigift@qq.com>
2024-04-17 10:57:57 +0800

    类型定义

--- src/apps/GreedySnake/index.vue
+++ src/apps/GreedySnake/index.vue
@@ -2,25 +2,18 @@
   <div class="outer-div">
     <div class="inner-div">
       <div v-for="(row, rowIndex) in snake.map" :key="rowIndex" class="row">
-        <span v-for="(item, colIndex) in row" :key="colIndex" class="cell" :class="{
-          isActive: item === 1,
-          isFood: item === 2,
-        }">{{ item }}
-        </span>
+        <span v-for="(item, colIndex) in row" :key="colIndex" class="cell" :class="snake.getClass(item)"> {{ item }} </span>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {floor} from "lodash";
+import { floor } from "lodash";
 
-interface direction {
-  ArrowUp: any,
-  ArrowDown: any,
-  ArrowLeft: any,
-  ArrowRight: any,
-}
+type Direction = "ArrowUp" | "ArrowDown" | "ArrowLeft" | "ArrowRight";
+type Position = { x: number; y: number };
+type Control = Record<Direction, () => boolean>;
 
 const snake = (() => {
   const defaultMap = [
@@ -34,93 +27,101 @@ const snake = (() => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ]
-  const defaultPosition = {x: 0, y: 0} as { x: number; y: number }
-  const control = <direction>{
+  ];
+  const defaultPosition: Position = { x: 0, y: 0 };
+  const control: Control = {
     ArrowUp: () => {
-      const pos = s.position
-      let re = pos.y > 0
+      const pos = s.position;
+      let re = pos.y > 0;
       if (re) {
-        pos.y--
+        pos.y--;
       }
-      return re
+      return re;
     },
     ArrowDown: () => {
-      const pos = s.position
-      let re = pos.y < s.map.length - 1
+      const pos = s.position;
+      let re = pos.y < s.map.length - 1;
       if (re) {
-        pos.y++
+        pos.y++;
       }
-      return re
+      return re;
     },
     ArrowLeft: () => {
-      const pos = s.position
-      let re = pos.x > 0
+      const pos = s.position;
+      let re = pos.x > 0;
       if (re) {
-        s.position.x--
+        s.position.x--;
       }
-      return re
+      return re;
     },
     ArrowRight: () => {
-      const pos = s.position
-      let re = pos.x < s.map.length - 1
+      const pos = s.position;
+      let re = pos.x < s.map.length - 1;
       if (re) {
-        s.position.x++
+        s.position.x++;
       }
-      return re
+      return re;
     },
-  }
-  const motion = (key: keyof direction) => {
-    const pos = s.position
-    const result = control[key]()
-    const x = pos.x
-    const y = pos.y
-    if (!result) return
-    s.snakeBody.push({...pos})
+  };
+
+  const isDirectionKey = (key: string): key is Direction => {
+    const keys = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
+    return keys.includes(key);
+  };
+  const motion = (key: Direction) => {
+    const pos = s.position;
+    const result = control[key]();
+    const x = pos.x;
+    const y = pos.y;
+    if (!result) return;
+    s.snakeBody.push({ ...pos });
     let first = s.snakeBody[0];
-    if (s.map[y][x] === 2){
-      eat()
-    }else{
-      s.map[first.y][first.x] = 0
-      s.snakeBody.shift()
+    if (s.map[y][x] === 2) {
+      eat();
+    } else {
+      s.map[first.y][first.x] = 0;
+      s.snakeBody.shift();
     }
-    s.map[y][x] = 1
-    s.snakeBody.map(item => {
-      s.map[item.y][item.x] = 1
-
-    })
-  }
+    s.map[y][x] = 1;
+    s.snakeBody.map((item) => {
+      s.map[item.y][item.x] = 1;
+    });
+  };
+  const getClass = (item: number) => ({
+    isActive: item === 1,
+    isFood: item === 2,
+  });
   const eat = () => {
-    generateFood()
-  }
+    generateFood();
+  };
   const generateFood = () => {
-    const x = floor(Math.random() * 9)
-    const y = floor(Math.random() * 9)
-    console.log(y, x)
-    s.map[y][x] = 2
-  }
+    const x = floor(Math.random() * 9);
+    const y = floor(Math.random() * 9);
+    console.log(y, x);
+    s.map[y][x] = 2;
+  };
   const init = () => {
-    s.map[0][0] = 1
-    generateFood()
-  }
-  window.addEventListener('keydown', e => {
-    if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(e.key))
-      motion(e.key as keyof direction)
-  })
+    s.map[0][0] = 1;
+    generateFood();
+  };
+  window.addEventListener("keydown", (e) => {
+    if (isDirectionKey(e.key)) {
+      motion(e.key);
+    }
+  });
 
-  setInterval(() => {
-
-  }, 1000)
+  setInterval(() => {}, 1000);
 
   const s = reactive({
-    position: {...defaultPosition},
+    position: { ...defaultPosition },
     snakeBody: [defaultPosition],
-    toward: "ArrowLeft",
-    map: [...defaultMap]
-  })
-  init()
-  return s
-})()
+    toward: <Direction>"ArrowLeft",
+    map: [...defaultMap],
+    getClass,
+  });
+  init();
+  return s;
+})();
 </script>
 
 <style scoped lang="less">
