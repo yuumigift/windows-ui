bb558dc308aee38e46a5f05184ce4641ce4df7c1
悠米🍇 <yuumigift@qq.com>
2024-05-01 17:40:49 +0800

    -

--- src/apps/AdventureYuumi/classes/Player.ts
+++ src/apps/AdventureYuumi/classes/Player.ts
@@ -87,8 +87,8 @@ export class Player extends Entity {
         // 调用向右移动的函数
         moveRight();
       }
-    } else {
       // 如果视口在最左边
+    } else {
       // 如果矩形速度向左，则继续向左移动
       if (this.vx < 0) {
         this.rect.x += this.vx;
