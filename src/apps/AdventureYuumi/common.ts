d8dc1e776cc2a597bf3e520c7894f3de783202be
悠米🍇 <yuumigift@qq.com>
2024-05-01 10:39:37 +0800

    -

--- src/apps/AdventureYuumi/common.ts
+++ src/apps/AdventureYuumi/common.ts
@@ -12,7 +12,7 @@ export const PLAYER_SPEED_FORCE = 0.1;
 export const PLAYER_SPEED_MAX = 5;
 export const PLAYER_JUMP_FORCE = 3;
 // monster
-export const MONSTER_MOVE_WAITING = 500;
+export const MONSTER_MOVE_WAITING = 300;
 export const MONSTER_JUMP_WAITING = 200;
 export const MONSTER_WIDTH = 18;
 export const MONSTER_HEIGHT = 18;
